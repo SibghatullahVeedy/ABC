bd034e731703bddcbe88a6a7412bca8e1c0bd212
Sibghatullah Veedy <sibghatullahveedy@gmail.com>
2020-07-14 16:44:45 -0700

    First Commit By Git

---
+++ GitExercise/src/main/java/ClassExercise/ClassExercise/App.java
@@ -0,0 +1,13 @@
+package ClassExercise.ClassExercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
